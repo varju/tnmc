e18ffd1060e2ca8134fb29683daf91d227e8e205
Alex Varju <alex@varju.ca>
2000-02-25 07:13:49 +0000

    script to archive the code from cvs
    git-svn-id: file:///tmp/svnrepo/trunk/tnmc@13 cf5a613f-8eca-db11-8eb1-000ea64e904d

---
+++ source/cron/archive_source.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -w
+
+use POSIX qw(strftime);
+
+my $base = "/usr/local/apache/tnmc";
+my $cvs = "/var/db/tnmc-cvs";
+
+my $tmp = $$;
+while (-d "/tmp/$tmp") {$tmp++;}
+
+mkdir("/tmp/$tmp",504);
+chdir("/tmp/$tmp");
+system("cvs -d $cvs export -rHEAD tnmc");
+system("tar cvzf tnmc.tar.gz tnmc");
+
+chdir($base);
+if (-e "source/tnmc.tar.gz") {
+    my @file_status = stat("source/tnmc.tar.gz");
+    my $lastmod = strftime "%Y%m%d", gmtime $file_status[9];
+    rename("source/tnmc.tar.gz","source/tnmc-$lastmod.tar.gz");
+}
+rename("/tmp/$tmp/tnmc.tar.gz","source/tnmc.tar.gz");
+
+system("rm -rf /tmp/$tmp");
+
