8c28970bc2b6c39d1b0c82562248ac58cc4669fa
Scott Thompson <scottt@interchg.ubc.ca>
2000-09-20 21:49:06 +0000

    added alex's feature request of a clickable link for the winning movie.
    git-svn-id: file:///tmp/svnrepo/trunk/tnmc@87 cf5a613f-8eca-db11-8eb1-000ea64e904d

--- movies/MOVIES.pl
+++ movies/MOVIES.pl
@@ -184,7 +184,12 @@ sub show_current_movie
 
                         <TR>
                         <TD nowrap><B>Movie: </TD>
-                        <TD nowrap>$movie{'title'}</TD>
+                        <TD nowrap><a href="javascript:window.open(
+                                                '/movies/movie_view.cgi?movieID=$current_movie',
+                                                'ViewMovie',
+                                                'resizable,height=350,width=450');
+                                                index.cgi
+                                           ">$movie{'title'}</a></TD>
                         </TR>
                         
                         <TR>
