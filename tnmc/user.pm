ec42292c8fed34287b545c8ead87d895f5018203
Alex Varju <alex@varju.ca>
2000-10-20 04:02:58 +0000

    fix spacing
    git-svn-id: file:///tmp/svnrepo/trunk/tnmc@168 cf5a613f-8eca-db11-8eb1-000ea64e904d

--- tnmc/user.pm
+++ tnmc/user.pm
@@ -25,60 +25,59 @@ use vars qw(@ISA @EXPORT @EXPORT_OK);
 #
 
 sub set_user{
-	my (%user, $junk) = @_;
-	my ($sql, $sth, $return);
-	
-	&db_set_row(\%user, $dbh_tnmc, 'Personal', 'userID');
-	
-	###############
-	### Return the User ID
-	
-	$sql = "SELECT userID FROM Personal WHERE username = '$user{username}'";
-	$sth = $dbh_tnmc->prepare($sql) or die "Can't prepare $sql:$dbh_tnmc->errstr\n";
-	$sth->execute;
-	($return) = $sth->fetchrow_array();
-	$sth->finish;
-	
-	return $return;
+    my (%user, $junk) = @_;
+    my ($sql, $sth, $return);
+    
+    &db_set_row(\%user, $dbh_tnmc, 'Personal', 'userID');
+    
+    ###############
+    ### Return the User ID
+    
+    $sql = "SELECT userID FROM Personal WHERE username = '$user{username}'";
+    $sth = $dbh_tnmc->prepare($sql) or die "Can't prepare $sql:$dbh_tnmc->errstr\n";
+    $sth->execute;
+    ($return) = $sth->fetchrow_array();
+    $sth->finish;
+    
+    return $return;
 }
 
 sub del_user{
-	my ($userID) = @_;
-	my ($sql, $sth, $return);
-	
-	###############
-	### Delete the user
-	
-	$sql = "DELETE FROM Personal WHERE userID = '$userID'";
-	$sth = $dbh_tnmc->prepare($sql) or die "Can't prepare $sql:$dbh_tnmc->errstr\n";
-	$sth->execute;
-	$sth->finish;
-	
+    my ($userID) = @_;
+    my ($sql, $sth);
+    
+    ###############
+    ### Delete the user
+    
+    $sql = "DELETE FROM Personal WHERE userID = '$userID'";
+    $sth = $dbh_tnmc->prepare($sql) or die "Can't prepare $sql:$dbh_tnmc->errstr\n";
+    $sth->execute;
+    $sth->finish;
 }
 
 sub get_user{
-	my ($userID, $user_ref, $junk) = @_;
-	my ($condition);
+    my ($userID, $user_ref, $junk) = @_;
+    my ($condition);
 
-	$condition = "(userID = '$userID' OR username = '$userID')";
-	&db_get_row($user_ref, $dbh_tnmc, 'Personal', $condition);
+    $condition = "(userID = '$userID' OR username = '$userID')";
+    &db_get_row($user_ref, $dbh_tnmc, 'Personal', $condition);
 }
 
 sub list_users{
-	my ($user_list_ref, $where_clause, $by_clause, $junk) = @_;
-	my (@row, $sql, $sth);
+    my ($user_list_ref, $where_clause, $by_clause, $junk) = @_;
+    my (@row, $sql, $sth);
 
-	@$user_list_ref = ();
+    @$user_list_ref = ();
 
-	$sql = "SELECT userID from Personal $where_clause $by_clause";
-	$sth = $dbh_tnmc->prepare($sql) or die "Can't prepare $sql:$dbh_tnmc->errstr\n";
-	$sth->execute;
-	while (@row = $sth->fetchrow_array()){
-		push (@$user_list_ref, $row[0]);
-	}
-	$sth->finish;
+    $sql = "SELECT userID from Personal $where_clause $by_clause";
+    $sth = $dbh_tnmc->prepare($sql) or die "Can't prepare $sql:$dbh_tnmc->errstr\n";
+    $sth->execute;
+    while (@row = $sth->fetchrow_array()){
+        push (@$user_list_ref, $row[0]);
+    }
+    $sth->finish;
 
-	return $#$user_list_ref;
+    return $#$user_list_ref;
 }
 
 1;
