fa1dff509f6a2e92f75b50c159a936885d487d28
Alex Varju <alex@varju.ca>
2000-10-20 04:20:49 +0000

    clean up spacing
    git-svn-id: file:///tmp/svnrepo/trunk/tnmc@169 cf5a613f-8eca-db11-8eb1-000ea64e904d

--- tnmc/template.pm
+++ tnmc/template.pm
@@ -27,181 +27,146 @@ use vars qw(@ISA @EXPORT @EXPORT_OK);
 #
 
 sub header{
-
-	&db_connect();
-	
-	# header and title
-	print "Content-type: text/html\n\n";
-
-	&get_cookie();
-
-	my $username = $USERID{'username'};
-
-	print qq{
-
-
+    &db_connect();
+    
+    # header and title
+    print "Content-type: text/html\n\n";
+    
+    &get_cookie();
+    
+    my $username = $USERID{'username'};
+    
+    print qq{
 <HTML>
 <HEAD>
 
-
 <style>
-
 p, ul, td, th, h1,h2,h3,h4,h5,h6, font, b, i, a {
-        font-family: verdana, helvetica, arial, sans-serif;
+    font-family: verdana, helvetica, arial, sans-serif;
 }
 
 p, ul, td, th {
-        font-size: 8pt;
+    font-size: 8pt;
 }
 
-th{
-	font-weight: bold;
-	background: #cccccc;
-	text-align: left;
-	color: #888888;
+th {
+    font-weight: bold;
+    background: #cccccc;
+    text-align: left;
+    color: #888888;
 }
 
 .menulink {
-        font-family: verdana, helvetica, arial, sans-serif;
-        font-size: 8pt;
-	color: #00067F;
-        text-decoration: none;
+    font-family: verdana, helvetica, arial, sans-serif;
+    font-size: 8pt;
+    color: #00067F;
+    text-decoration: none;
 }
-
 </style>
-
 	
-	<TITLE>TNMC Online</TITLE>
+<TITLE>TNMC Online</TITLE>
 </HEAD>
 
 <BODY BACKGROUND="/template/bg.gif">
 <center>
 
-<!-- 
-    <table height="100%" border="0">
-    <tr><td valign="middle">
--->
-
-
 <TABLE BORDER="0" CELLPADDING=0 CELLSPACING=0 background="">
 <TR>
-		<TD colspan="4" bgcolor="2266aa" BACKGROUND="/template/top_center_bg.gif"><IMG width="290" height="36" SRC="/template/logo/basic.gif"></TD>
-		<TD ALIGN="left" bgcolor="2266aa" BACKGROUND="/template/top_center_bg.gif"><FONT style="font-size: 20pt" COLOR=#FFFFFF FACE="verdana" SIZE="+2"
-		><B>$username</B></FONT>&nbsp;</TD>
-	
-	<TD><IMG SRC="/template/top_right.gif"></TD>
-
+  <TD colspan="4" bgcolor="2266aa" BACKGROUND="/template/top_center_bg.gif"><IMG width="290" height="36" SRC="/template/logo/basic.gif"></TD>
+  <TD ALIGN="left" bgcolor="2266aa" BACKGROUND="/template/top_center_bg.gif"><FONT style="font-size: 20pt" COLOR="#FFFFFF" FACE="verdana" SIZE="+2">
+    <B>$username</B></FONT>&nbsp;</TD>
+  <TD><IMG SRC="/template/top_right.gif"></TD>
 </TR>
 
 <TR valign="top">
-	<TD bgcolor="ffffff" BACKGROUND="/template/body_bg.gif">&nbsp;</td>
-	<TD bgcolor="ffffff"><br>
-
-
-	};
+  <TD bgcolor="ffffff" BACKGROUND="/template/body_bg.gif">&nbsp;</td>
+  <TD bgcolor="ffffff"><br>
+    };
 }
 
 ###################################################################
 sub footer{
+    my ($pageID) = @_;
+    
+    &db_connect();
+    
+    print q{
+  <br></TD>
 
-	my ($pageID) = @_;
+  <TD bgcolor="ffffff">
+    <img src="/template/blank_dot.gif" width="13" height="1"></td>
+  <TD bgcolor="ffff88" background="/template/menu_bg.gif">
+    <img src="/template/blank_dot.gif" width="13"></td>
+  <TD bgcolor="ffff88" background="/template/menu_bg_cp.gif" VALIGN="TOP">
+    <FONT style="text-decoration:none;" COLOR="#00067F" FACE="verdana" SIZE="-2">
+    };
+    
+    if (!$pageID)
+    {
+        if ($LOGGED_IN) {
+            new_nav_menu();
+        }
+        else {
+            new_nav_login();
+        }
+    }
+    else {
+        print "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<br>";
+    }
+    
+    print qq{
+    </font>
+  </TD>
 
-	&db_connect();
-
-	print q{
-
-
-
-
-	<br></TD>
-
-	<TD bgcolor="ffffff"><img src="/template/blank_dot.gif"
-width="13" height="1"></td>
-	<TD bgcolor="ffff88" background="/template/menu_bg.gif"><img
-src="/template/blank_dot.gif" width="13"></td>
-	
-	<TD bgcolor="ffff88" background="/template/menu_bg_cp.gif" 
-VALIGN="TOP">
-
-		<FONT style="text-decoration:none;" COLOR="#00067F" FACE="verdana"
-SIZE="-2">
-
-
-	};
-
-	if (!$pageID)
-	{
-		if ($LOGGED_IN)
-		{ 	new_nav_menu();
-		}
-		else
-		{	new_nav_login();
-		}
-	}
-	else
-	{	print "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<br>";
-	}
-
-	print qq{
-
-	</font>
-	</TD>
-	
-	<TD bgcolor="ffff88" background="/template/body_right_bg2.gif">&nbsp;</TD>
-	
+  <TD bgcolor="ffff88" background="/template/body_right_bg2.gif">&nbsp;</TD>
 </TR>
 
-
-
 <TR>
-	<td BACKGROUND="/template/bottom_bg.gif"><img src="/template/bottom_left2.gif"></td>
-	<td bgcolor="888888" colspan="2" BACKGROUND="/template/bottom_bg_cp.gif"><FONT FACE="verdana" SIZE="-2">
-		<a href="/" style="text-decoration:none"><font color="000000">$tnmc_url</a>
-	</FONT></td>
-	<TD bgcolor="888888" BACKGROUND="/template/menu_bottom_bg.gif">&nbsp;</TD>
-	<TD bgcolor="888888" BACKGROUND="/template/menu_bottom_bg_cp.gif"><FONT FACE="verdana" SIZE="-2">
-	};
-	
-	if (!$pageID) 
-	{ 	print qq 
-		{	<a href="/site_info.cgi" style="text-decoration:none"><font color="#000000">site info</font></a>
-		};
-	}
-	else { print "<BR>"; }
+  <td BACKGROUND="/template/bottom_bg.gif">
+    <img src="/template/bottom_left2.gif"></td>
+  <td bgcolor="888888" colspan="2" BACKGROUND="/template/bottom_bg_cp.gif">
+    <FONT FACE="verdana" SIZE="-2">
+    <a href="/" style="text-decoration:none"><font color="000000">$tnmc_url</a>
+    </FONT></td>
+  <TD bgcolor="888888" BACKGROUND="/template/menu_bottom_bg.gif">&nbsp;</TD>
+  <TD bgcolor="888888" BACKGROUND="/template/menu_bottom_bg_cp.gif">
+    <FONT FACE="verdana" SIZE="-2">
+    };
 
-	print q{
-	</FONT></TD>
-	
-	<TD><IMG SRC="/template/bottom_right.gif"></TD>
+    if (!$pageID) {
+        print q{
+    <a href="/site_info.cgi" style="text-decoration:none"><font color="#000000">site info</font></a>
+	};
+    }
+    else { 
+        print "<BR>\n"; 
+    }
+
+    print q{
+    </FONT></TD>
+
+  <TD><IMG SRC="/template/bottom_right.gif"></TD>
 </tr>
 </TABLE>
 
-<!--
-    </td></tr></table>
--->
 <br>
 
 </BODY>
-
 </HTML>
-
-
-	
-
-	};
-
-	&db_disconnect();
+    };
+    
+    &db_disconnect();
 }
 
 ###################################################################
 sub show_heading{
-	my ($heading_text, $colour, $junk) = @_;
-	print qq{
-		<table border="0" cellpadding="1" cellspacing="0" 
-bgcolor="448800" width="100%">
-		<tr><td nowrap>&nbsp;<font color="ffffff"><b>
-		$heading_text</b></font></td></tr></table>
-	};
+    my ($heading_text) = @_;
+    print qq{
+    <table border="0" cellpadding="1" cellspacing="0" bgcolor="448800" width="100%">
+      <tr><td nowrap>&nbsp;<font color="ffffff"><b>
+      $heading_text</b></font></td></tr>
+    </table>
+    };
 }
 
 1;
