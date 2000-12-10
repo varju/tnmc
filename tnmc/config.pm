0c9be1daa24a43ac472007aba712bb195c9be44f
Alex Varju <alex@varju.ca>
2000-12-10 03:12:45 +0000

    double oops
    git-svn-id: file:///tmp/svnrepo/trunk/tnmc@316 cf5a613f-8eca-db11-8eb1-000ea64e904d

--- tnmc/config.pm
+++ tnmc/config.pm
@@ -20,6 +20,6 @@ use vars qw(@ISA @EXPORT @EXPORT_OK $tnmc_hostname $tnmc_url $tnmc_email $tnmc_d
 $tnmc_hostname = 'www.tnmc.ca';
 $tnmc_domain = '.tnmc.ca';
 $tnmc_url = "http://$tnmc_hostname";
-$tnmc_email = 'alex@varju.bc.ca';
+$tnmc_email = 'tnmc-list@interchange.ubc.ca';
 
 1;
