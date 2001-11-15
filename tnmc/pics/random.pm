9e6bbdcfce6c8cd6b91dba7d39f50cdfdfd3d864
Scott Thompson <scottt@interchg.ubc.ca>
2001-11-15 21:34:45 +0000

    module performance improvements - part 1b
    git-svn-id: file:///tmp/svnrepo/trunk/tnmc@525 cf5a613f-8eca-db11-8eb1-000ea64e904d

---
+++ tnmc/pics/random.pm
@@ -0,0 +1,68 @@
+package tnmc::pics::random;
+
+use strict;
+
+#
+# module configuration
+#
+BEGIN {
+    use tnmc::db;
+    use tnmc::pics::pic;
+}
+
+#
+# module routines
+#
+
+sub show_random_pic{
+    my ($offset) = @_;
+    
+    my $sql = "SELECT DATE_FORMAT(NOW(), '%m%j%H%i')";
+    my $sth = $dbh_tnmc->prepare($sql); 
+    $sth->execute();
+    my ($seed) = $sth->fetchrow_array();
+    $seed = int ($seed / 10);
+    my $picID = &get_random_pic($seed, $offset);
+    
+    my %pic;
+    &get_pic($picID, \%pic);
+    
+    my $pic_img = &get_pic_url($picID, ['mode'=>'thumb']);
+    $pic{description} &&= " ($pic{description})";
+    my $date = $pic{timestamp};
+    $date =~ s/\s.*//;
+    my $pic_url = "/pics/search_slide.cgi?search=date-span&search_from=$date+00%3A00&search_to=$date+23%3A59%3A59&picID=$picID";
+    print qq{
+        <a href="$pic_url"><img src="$pic_img" width="80" height="64" border="0" alt="$pic{title}$pic{description}"></a>
+    };
+    
+}
+
+# to-do: make this function cache stuff in the general config table
+sub get_random_pic{
+    my ($seed, $offset) = @_;
+    
+    my $sql = "SELECT count(*) FROM Pics WHERE typePublic = '1'";
+    my $sth = $dbh_tnmc->prepare($sql); 
+    $sth->execute();
+    my ($count_pics) = $sth->fetchrow_array();
+    
+    if ($seed){
+        srand($seed);
+    }
+    
+    my $index;
+    $offset++;
+    while ($offset--){
+        $index = int(rand($count_pics/100)) . int(rand(100)) ;
+    }
+    
+    $sql = "SELECT picID FROM Pics WHERE typePublic = '1' LIMIT $index, 1";
+    $sth = $dbh_tnmc->prepare($sql);
+    $sth->execute();
+    my ($picID) = $sth->fetchrow_array();
+     
+    return $picID;
+}
+
+1;
