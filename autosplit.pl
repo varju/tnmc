e8e13759a8fe5e592e36e631762d4e1bce7b8e0a
Scott Thompson <scottt@interchg.ubc.ca>
2002-01-15 02:49:18 +0000

    use explicit paths so can call from any directory
    git-svn-id: file:///tmp/svnrepo/trunk/tnmc@579 cf5a613f-8eca-db11-8eb1-000ea64e904d

--- autosplit.pl
+++ autosplit.pl
@@ -3,11 +3,11 @@
 use AutoSplit;
 use strict;
 
-my @modules = `find tnmc -type f | grep -v CVS | grep -v \\~ | grep \\.pm\\\$`;
+my @modules = `find /tnmc/tnmc -type f | grep -v CVS | grep -v \\~ | grep \\.pm\\\$`;
 
 print "Autospliting $#modules modules...\n";
 
 foreach my $module (@modules){
     chomp $module;
-    autosplit($module, "./auto/", 0, 1, 1);
+    autosplit($module, "/tnmc/auto/", 0, 1, 1);
 }
