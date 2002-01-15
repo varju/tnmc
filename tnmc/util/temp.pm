4dc26d4fccead2967b002337157ae8f3bb9ed024
Scott Thompson <scottt@interchg.ubc.ca>
2002-01-15 02:48:14 +0000

    fix up temp dir so that it's really a part of tnmc
    git-svn-id: file:///tmp/svnrepo/trunk/tnmc@578 cf5a613f-8eca-db11-8eb1-000ea64e904d

--- tnmc/util/temp.pm
+++ tnmc/util/temp.pm
@@ -4,8 +4,8 @@ use strict;
 use tnmc::util::file;
 
 BEGIN{
-    use vars qw($temp_path);
-    $temp_path = "/tmp/"
+    use vars qw($temp_dir);
+    $temp_dir = "/tmp/"
 }
 
 #
@@ -17,15 +17,15 @@ sub get_dir{
     my $PID = $$;
     
     # get a unique name
-    my $dir = $lang::config::temp_dir . 'tnmc_' . $PID . '/';
+    my $dir = $temp_dir . 'tnmc_' . $PID . '/';
     my $i;
     while (-e $dir) {
         $i++;
-        $dir = $lang::config::temp_dir . 'tnmc_' . $PID . '_' . $i . '/';
+        $dir = $temp_dir . 'tnmc_' . $PID . '_' . $i . '/';
     }
     
     # make the dir
-    &lang::util::file::make_directory($dir);
+    &tnmc::util::file::make_directory($dir);
     
     # return the dir name
     return $dir;
@@ -35,10 +35,10 @@ sub kill_dir{
     my ($dir) = @_;
     
     # make sure it's actually in the data/temp dir.
-    return 0 if ($dir !~ /^$lang::config::temp_dir\/tnmc/);
+    return 0 if ($dir !~ /^$temp_dir\/tnmc/);
     
     # delete the dir
-    &lang::util::file::kill_tree($dir);
+    &tnmc::util::file::kill_tree($dir);
     
     return 1;
 }
