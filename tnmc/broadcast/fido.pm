f79548ba6ec05abc4137f580699a845e5f3c0d45
Scott Thompson <scottt@interchg.ubc.ca>
2002-04-06 03:27:29 +0000

    fix fido sms
    git-svn-id: file:///tmp/svnrepo/trunk/tnmc@588 cf5a613f-8eca-db11-8eb1-000ea64e904d

--- tnmc/broadcast/fido.pm
+++ tnmc/broadcast/fido.pm
@@ -44,21 +44,59 @@ sub sms_send_fido{
 
     ### Build the argument string.
     my $SEND = substr($msg, 0, 160);
-    my $URL = 'http://fido.globewebs.com/cgi-fido/sms.cgi';
-    
+    my $URL = 'http://nokia.zimismobile.com/scripts/zimcgic.exe';
     ### Get a User agent
     my $ua = new LWP::UserAgent;
     $ua->agent("AgentName/01 " . $ua->agent);
     
     ### Make the Request
     my $req = POST $URL,
-    [ 'areacode' => $areacode,
-      'address' => $phone,
+    [ 'connection' => 'smooch',
+      'program' => 'psendcagenmess',
+      'template' => '(AreaCode, PhoneNumber,message,emoticon)',
+      'AreaCode' => $areacode,
+      'PhoneNumber' => $phone,
       'message' => $SEND,
-      'total' => length($SEND),
+      'emoticon' => "[TNMC]",
       ];
     $req->content_type('application/x-www-form-urlencoded');
+    my $res = $ua->request($req);
+#    print STDERR "====\n\n";
+#    print STDERR $res->as_string();
+    return $res;
+    return $ua->request($req);
+}
 
+sub sms_send_fido_microcell{
+    my ($phone, $msg, $junk) = @_;
+    
+    ### see if we actually want to send anything.
+    if (length($msg) == 0){
+        return 0;       # nope.
+    }
+    
+    ### get the areacode, if they have one.
+    my $areacode = phone_get_areacode($phone);
+    $phone = phone_get_localnum($phone);
+
+    ### Build the argument string.
+    my $SEND = substr($msg, 0, 160);
+    my $URL = 'http://www.fido.ca/NASApp/info/HomeFrame/sendmessage.jsp';
+    ### Get a User agent
+    my $ua = new LWP::UserAgent;
+    $ua->agent("AgentName/01 " . $ua->agent);
+    
+    ### Make the Request
+    my $req = POST $URL,
+    [ 'phone' => $areacode . $phone,
+      'name' => "tnmc",
+      'text' => $SEND,
+      ];
+    $req->content_type('application/x-www-form-urlencoded');
+    my $res = $ua->request($req);
+#    print STDERR "====\n\n";
+#    print STDERR $res->as_string();
+    return $res;
     return $ua->request($req);
 }
 
